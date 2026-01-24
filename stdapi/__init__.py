b4e8cd1d9ec16a80da533e969470981187a9379d
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-01-24 11:43:37 +0530

    Update __init__.py

--- stdapi/__init__.py
+++ stdapi/__init__.py
@@ -1,7 +1,10 @@
 from .media import download as media_download
+from .client import StdAPIClient, StdAPIError
 
 __all__ = [
     "media_download",
+    "StdAPIClient",
+    "StdAPIError",
 ]
 
 __version__ = "0.0.1"
