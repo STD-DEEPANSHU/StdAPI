0716ede75c5d4706e81f6520935ae237c1940d2f
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-18 21:10:22 +0530

    Update exceptions.py

--- stdapi/exceptions.py
+++ stdapi/exceptions.py
@@ -1,3 +1,18 @@
 class StdAPIError(Exception):
     """Base exception for StdAPI"""
-    pass
+
+    def __init__(self, message, status_code=None, response=None):
+        super().__init__(message)
+        self.status_code = status_code
+        self.response = response
+
+    def __str__(self):
+        msg = super().__str__()
+
+        if self.status_code:
+            msg += f" | Status: {self.status_code}"
+
+        if self.response:
+            msg += f" | Response: {self.response[:150]}"
+
+        return msg
