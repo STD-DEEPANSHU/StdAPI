e6eb72b12cc145f0aa486848f6f232521baf794e
STD DEEPANSHU <admin@deepanshu.in>
2026-09-20 11:03:42 +0530

    std deepanshu - v2.0.1: Clean temporary build script

--- push.bat
+++
@@ -1,6 +0,0 @@
-git config user.name "STD DEEPANSHU"
-git config user.email "admin@deepanshu.in"
-git add .
-git commit -m "std deepanshu - v2.0.0: Connected live StdApiBackend Master Gateway, Universal Media & Tools Pipeline"
-git push https://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@github.com/STD-DEEPANSHU/StdAPI.git main
-git remote set-url origin https://github.com/STD-DEEPANSHU/StdAPI.git
